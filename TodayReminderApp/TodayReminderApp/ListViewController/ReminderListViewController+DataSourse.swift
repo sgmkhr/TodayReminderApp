2a45635a55e28616492de00ff8c312f0cc60b343
sgmkhr <mksghr10@gmail.com>
2024-09-29 15:08:29 +0900

    Format the date and time & Organize view controller

---
+++ TodayReminderApp/TodayReminderApp/ListViewController/ReminderListViewController+DataSourse.swift
@@ -0,0 +1,22 @@
+//
+//  ReminderListViewController+DataSourse.swift
+//  TodayReminderApp
+//
+//  Created by 菅原実希 on 2024/09/29.
+//
+
+import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+    }
+}
